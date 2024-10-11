cfd8748797f2e346eade507d368276d0f9e3971e
nickrauchtnicht <nicolas.rauth@bse.eu>
2024-10-11 16:43:04 +0200

    Blabklbka

--- hw3.py
+++ hw3.py
@@ -40,13 +40,23 @@ def safe_subtract(a, b):
         # For any other type of issue, print the error and return None
         print(f"An error occurred: {e}")
 
-# Imagine you have a dictionary with the attributes of a person
+#3)
+#  Imagine you have a dictionary with the attributes of a person
 # {'name': 'John', 'last_name': 'Doe', 'birth': 1987}
 # {'name': 'Janet', 'last_name': 'Bird', 'gender': 'female'}
 # create two functions that returns the age of the person
 # that handles both examples.
 # Name the first function "retrieve_age_eafp" and follow EAFP
 # Name the second function "retrieve_age_lbyl" and follow lbyl
+from datetime import datetime
+
+def retrieve_age_eafp(person):
+    current_year = datetime.now().year
+    try:
+        birth_year = person['birth']
+        return current_year - birth_year
+    except KeyError:
+        return "Birth year is not available."
 
 # 4)
 # Imagine you have a file named data.csv. 
@@ -55,6 +65,14 @@ def safe_subtract(a, b):
 # that it might not exist. 
 #
 
+def read_data(file_path="data.csv"):
+    try:
+        with open(file_path, 'r') as file:
+            data = file.read()
+            return data
+    except FileNotFoundError:
+        print(f"The file {file_path} does not exist.")
+        return None
 
 # 5) Squash some bugs! 
 # Find the possible logical errors (bugs) 
@@ -66,19 +84,40 @@ for elem in [10, 5, 2]:
     double = elem * 2
     total_double_sum += elem
 
+total_double_sum = 0
+for elem in [10, 5, 2]:
+    double = elem * 2
+    total_double_sum += double  # Add double instead of elem
+
+
 ### (b)
 strings = ''
 for string in ['I', 'am', 'Groot']:
     strings = string+"_"+string
+
+strings = ''
+for string in ['I', 'am', 'Groot']:
+    strings += string + "_"  # Accumulate the strings with an underscore
+strings = strings.rstrip('_')  # Remove the last unnecessary underscore
 
 ### (c) Careful!
 j=10
 while j > 0:
    j += 1
 
+j = 10
+while j > 0:
+    j -= 1  # Decrement j to eventually break the loop
+
+
 ### (d)
 productory = 0
 for elem in [1, 5, 25]:
     productory *= elem
+
+productory = 1  # Start with 1 for a product
+for elem in [1, 5, 25]:
+    productory *= elem
+
 
 
